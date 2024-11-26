520190a79d892467bafbce967ae66ad2e7ddca7f
Sravya N <snagalak@purdue.edu>
2024-11-25 18:39:41 -0800

    Testing vercel

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";  
-import records from "./routes/record.js";
+import records from "../routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -14,8 +14,6 @@ mongoose.connect(mongoURI)
 app.use(cors());
 app.use(express.json())
 app.use("/record", records);
-
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
