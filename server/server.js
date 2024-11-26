ca73ccf239389c8b63b9da64b402880170f6c0fa
Sravya N <snagalak@purdue.edu>
2024-11-25 17:40:00 -0800

    Azure

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+const path = require("path");
 
 
 const mongoURI = process.env.ATLAS_URI; 
@@ -13,8 +14,12 @@ mongoose.connect(mongoURI)
 .catch(err => console.error("MongoDB connection error:", err));
 
 app.use(cors());
-app.use(express.json());
+app.use(express.static("./client/dist"));
 app.use("/record", records);
+app.get("*",(req,res)=> {
+  res.sendFile(path.resolve(__dirname,"client","dist","index.html"))
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
